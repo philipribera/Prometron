fac0a6eb96f955d8ff1fc648090cafae5281483c
phiribribera <philip.ribera@student.kyh.se>
2019-02-28 13:41:37 +0100

    fixed user description on Account Page

--- src/components/DescriptionChange/index.js
+++ src/components/DescriptionChange/index.js
@@ -1,30 +1,51 @@
 import React, { Component } from 'react';
 import { withFirebase } from '../Firebase';
+//import { isNullOrUndefined } from 'util';
+
+const styles = {
+    display: "inline"
+}
 
 class DescriptionChangeForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
             description: "",
-        };
+            // ADDED, PR
+            userData: {},
+            edit: false
+        };       
     }
 
-
-    
-    onChange = event => {
-        this.setState({ [event.target.name]: event.target.value })
+    onSubmit = (e) => {
         this.props.firebase.user(this.props.userId).child("description").set(this.state.description)
     };
 
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+        
+    };
+
+    // render usernamechange component
+    changeDesc = (e) => {
+        let inputValue = document.getElementById('input-desc').value;        
+        this.state.edit ? this.setState({ edit: false }) : this.setState({ edit: true });
+        this.setState({ description: inputValue });        
+    };
+
+    componentWillUnmount() {
+        this.props.firebase.user(this.props.userId).child("description").set(this.state.description);
+    }
+        
     render() {
         return (
-            <form onSubmit={this.onSubmit}>
-                <input
-                    name= "description"
-                    value= {this.state.description}
+            <form onSubmit={this.onSubmit} style={styles}>
+                <input id="input-desc" maxLength="18"
+                    name="description"
+                    value={this.state.description}
                     onChange={this.onChange}
                     type="text"
-                />
+                />                
             </form>
         );
     };
